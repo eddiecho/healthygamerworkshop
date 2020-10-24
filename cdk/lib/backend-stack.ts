72e577c7c39a86828ff06cf64129b0abb7a3668c
Edward Cho <eunseocho@gmail.com>
2020-10-24 13:38:13 -0700

    temp remove domain name completely because the hashing setup is borked

--- cdk/lib/backend-stack.ts
+++ cdk/lib/backend-stack.ts
@@ -49,10 +49,13 @@ export class BackendStack extends Cdk.Stack {
     });
 
     this.api = new ApiGateway.RestApi(this, 'HealthyGamerWorkshopApi', {
+      // CDK generation is kind of dumb, remove then readd 
+      /* 
       domainName: {
         domainName: `api.${this.props.domainName}`,
         certificate: certificate,
       },
+      */
       defaultCorsPreflightOptions: {
         allowOrigins: [ ApiGateway.Cors.ALL_ORIGINS ],
         allowCredentials: true,
